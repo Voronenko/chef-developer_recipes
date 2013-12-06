4ee33869f61b93d45c2d143e908064b75b53bfd2
Vyacheslav <voronenko@gmail.com>
2013-12-06 22:55:35 +0200

    Initial set of recipes

---
+++ recipes/default.rb
@@ -0,0 +1,9 @@
+# --- Install packages we need ---
+package 'gpick'
+package 'xclip'
+package 'nautilus-open-terminal'
+
+include_recipe "googlechrome"
+include_recipe "webmin"
+#include_recipe "rabbitvcs"
+
