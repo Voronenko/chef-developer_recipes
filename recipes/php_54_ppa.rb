e07aa1e5dc0bb92e3f25191a33bd28a2463106a6
Vyacheslav Voronenko <voronenko@gmail.com>
2014-08-15 16:05:35 +0300

    Introduced support for PHP54 & PHP55 rather than default PHP53 php version https://launchpad.net/~ondrej/+archive/ubuntu/php5

---
+++ recipes/php_54_ppa.rb
@@ -0,0 +1,11 @@
+apt_repository "php54" do
+  uri "http://ppa.launchpad.net/ondrej/php5-oldstable/ubuntu"
+  distribution node['lsb']['codename']
+  components ["main"]
+  keyserver "keyserver.ubuntu.com"
+  key "E5267A6C"
+end
+
+include_recipe "apt"
+
+ 
