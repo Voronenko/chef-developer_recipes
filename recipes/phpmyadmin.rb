cc959e55093c7b5d326a576174c239cda365e1c6
Vyacheslav Voronenko <voronenko@gmail.com>
2014-08-26 23:19:59 +0300

    dual apache conf style support (issues with fresh opscode 12.04 image)

--- recipes/phpmyadmin.rb
+++ recipes/phpmyadmin.rb
@@ -8,7 +8,13 @@ end
 package "phpmyadmin"
 
 link "#{node['apache']['dir']}/conf.d/phpmyadmin.conf" do
-  to "/etc/phpmyadmin/apache.conf"  
+  to "/etc/phpmyadmin/apache.conf"
+  not_if { ::File.directory?("#{node['apache']['dir']}/conf-enabled") }  
 end
 
+
+link "#{node['apache']['dir']}/conf-enabled/phpmyadmin.conf" do
+  to "/etc/phpmyadmin/apache.conf"
+  not_if { ::File.directory?("#{node['apache']['dir']}/conf.d") }  
+end
 
